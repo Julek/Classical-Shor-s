d08b970da948479d9f341c89a4c9136ddbc645ec
Julian Sutherland <julian.sutherland10@imperial.ac.uk>
2012-07-30 10:08:28 +0100

    added user interface

--- Shor.hs
+++ Shor.hs
@@ -1,29 +1,43 @@
+import Control.Monad.Loops
+import Data.Char
+import Data.List
 import Data.Maybe
+import System.Environment
+import System.Exit
+import System.IO
 
 main :: IO ()
 main = do
-     putStr "Enter Number for factoring: "
-     s <- getLine
+     args <- getArgs
+     s <- if((not . null $ args) && (and . map isNumber . head $ args))
+          then return . head $ args
+          else  (if(not . null $ args)
+                            then putStrLn ((show . head $ args) ++ "is not a valid argument.")
+                            else return ()) >> iterateWhile (not . and . map isNumber) (putStr  "Enter Number for factoring: " >> hFlush stdout >> getLine
+                                                                                                          >>= (\ x -> if(not . and . map isNumber $ x)
+                                                                                                                                   then putStrLn ( show x ++ " is not an integer.") >> return x
+                                                                                                                                   else return x))
      let n = read s :: Integer
-         f = shor n
-     if(f == Nothing)
-          then putStrLn (show n ++ " is prime.")
-          else case (fromJust f) of
-                    (Left x) -> putStrLn ("factors of " ++ show n ++ " found: " ++ (show x))
-                    (Right x) -> putStrLn ("factors of " ++ show n ++ " found: " ++ (show x))
-     return ()
+         f = factor n
+     putStrLn (show n ++ ": " ++ (unwords . map show $ f))
      
-shor :: Integer -> Maybe (Either Integer (Integer, Integer))
+factor :: Integer -> [Integer]
+factor x = case f of
+                        (Just (f1, f2)) -> (factor f1) ++ (factor f2)
+                        Nothing -> [x]
+       where f = shor x
+
+shor :: Integer -> Maybe (Integer, Integer)
 shor n = listToMaybe . mapMaybe (shor' n) $ [2..(n - 1)]
 
-shor' :: Integer -> Integer -> Maybe (Either Integer (Integer, Integer))
+shor' :: Integer -> Integer -> Maybe (Integer, Integer)
 shor' n y = if(gcd y n /= 1)
-                           then Just (Left (gcd y n))
+                           then Just (gcd y n, n `div` gcd y n)
                            else if(order `mod` 2 /= 0)
                                        then Nothing
                                        else if(y^s `mod` n == (n - 1))
                                                         then Nothing
-                                                        else Just (Right (gcd (y ^ s - 1) n, gcd (y ^ s + 1) n))
+                                                        else Just (gcd (y ^ s - 1) n, gcd (y ^ s + 1) n)
                         where order = (+2) . length . takeWhile (/= 1) . map (\x -> y^x `mod` n) $ [2..(n - 1)]
                               s = order `div` 2
                                                         
